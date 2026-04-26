321b0009f56cee3bb5a5d98c63cf2b4ed6876a0f
MercierLucas <lmercier.78@gmail.com>
2026-04-26 10:14:33 -0400

    Adding OARSI

--- src/lib/publications.ts
+++ src/lib/publications.ts
@@ -26,6 +26,7 @@ export const abstracts:Publication[] = [
     title: 'Framework for the application of markerless motion capture to biomechanics',
     authors: ['Mercier, L.', 'Cresson, T.', 'Mezghani, N.', 'Vázquez, C.'],
     loc: 'Multidisciplinary Biomechanics Journal · 2025',
+    badges: [{ label: 'Oral', type: 'oral' }],
     links: [{ label: 'DOI', href: 'https://doi.org/10.46298/mbj.16163' }],
     image: '/images/publis/2025_sb_mercier.png'
 
@@ -35,19 +36,22 @@ export const abstracts:Publication[] = [
     authors: ['Naaïm, A.', 'Rozaire, J.', 'Mercier, L.', 'Begon, M.', 'Cherni, Y.'],
     loc: 'Multidisciplinary Biomechanics Journal · 2025',
     links: [{ label: 'DOI', href: 'https://doi.org/10.46298/mbj.16215' }],
+    badges: [{ label: 'Oral', type: 'oral' }],
     image: '/images/publis/2025_sb_naaim.png'
 
 },
-];
-
-export const presentations:Publication[] = [
 {
     title: 'Using knee kinesiography to train markerless pose estimation models for first-line kinematic assessment in knee osteoarthritis',
     authors: ['Mercier, L.', 'Marois, B.', 'Fuentes, A.', 'Cagnin, A.', 'Cresson, T.', 'Vazquez, C.'],
     loc: 'OARSI World Congress on Osteoarthritis · April 2026',
     badges: [{ label: 'Poster', type: 'poster' }],
+    links: [{ label: 'DOI', href: 'https://www.oarsijournal.com/article/S1063-4584(26)00235-9/abstract' }],
     image: '/images/publis/2026_oarsi.png'
 },
+];
+
+export const presentations:Publication[] = [
+
 {
     title: 'Evaluation of 3D marker-less motion capture precision in upper limb children movement',
     authors: ['Naaim, A.', 'Rozaire, J.', 'Mercier, L.', 'Duprey, S.', 'Begon, M.'],
